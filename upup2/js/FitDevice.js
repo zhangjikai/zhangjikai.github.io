8cff98837723c646ee14746b312671797d576eee
zhangjikai <zhangjikai9@163.com>
2015-09-20 15:46:48 +0800

    Update FitDevice.js

--- upup2/js/FitDevice.js
+++ upup2/js/FitDevice.js
@@ -29,6 +29,7 @@ function initCanvasWH(canvas) {
     if (isPhone) {
         canvas.width = parseInt(window.innerWidth * 0.98);
         canvas.height = parseInt(window.innerHeight * 0.7);
+        alert(canvas.height);
     } else {
         canvas.width = 300;
         canvas.height = 500;
