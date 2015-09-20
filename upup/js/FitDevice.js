a9dd96f917953b5fe22513e64db3682c7cad0b11
zhangjikai <zhangjk1993@gmail.com>
2015-09-20 15:30:05 +0800

    测试

--- upup/js/FitDevice.js
+++ upup/js/FitDevice.js
@@ -1,6 +1,11 @@
 /**
  * Created by zhangjk on 2015/9/20.
  */
+
+/**
+ * 判断是手机还是电脑访问
+ * @returns {boolean} true:手机 ; false 电脑
+ */
 function judgePlatform() {
     var system = {
         win: false,
@@ -22,12 +27,10 @@ function judgePlatform() {
 function initCanvasWH(canvas) {
     var isPhone = judgePlatform();
     if (isPhone) {
-        canvas.width = document.body.clientWidth;
-        canvas.height = document.body.clientHeight;
+        canvas.width = parseInt(window.innerWidth * 0.98);
+        canvas.height = parseInt(window.innerHeight * 0.9);
     } else {
         canvas.width = 300;
         canvas.height = 500;
     }
-
-
 }
